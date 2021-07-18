aa90750c7b0a67bcbb1644e416ff3888702e51d5
Manikandaprabu <massmanibrabu@gmail.com>
2021-07-18 22:59:41 +0530

    Mani

---
+++ src/org/mail/Nisha.java
@@ -0,0 +1,31 @@
+package org.mail;
+
+public class Nisha {
+
+	public static void main(String[] args) {
+
+		String n = "Nishakerala24@gmail.com";
+		String t = "8248586171l";
+
+		boolean r = n.contains("Nishakerala24@gmail.com");
+		if (r==true) {
+			System.out.println("True/False");
+		}
+		else {
+			System.out.println("False/True");
+		}
+		char s = t.charAt(1);
+		char l=s;
+		if (s==l) {
+			System.out.println("Invalid Number");
+		}
+		else {
+			System.out.println("Valid Number");
+		}
+		
+			
+		
+		
+
+	}
+}
